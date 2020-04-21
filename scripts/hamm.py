9afa0d1cf9bcc70ec0cc14567e096a1e720e04f1
Jonathan Wells <jnw72@cornell.edu>
2020-04-21 17:04:18 -0400

    Add comments

--- scripts/hamm.py
+++ scripts/hamm.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def hamming(seq1, seq2):
+    """Return the Hamming distance between two strings."""
     nbases = len(seq1)
+    # Here we just want to check if seqs are same length. If not, assert will
+    # fail and the program aborts
     assert nbases == len(seq2)
 
     hdist = 0
@@ -11,7 +14,9 @@ def hamming(seq1, seq2):
     return hdist
 
 if __name__ == '__main__':
+    # This little snippet just reads the two lines from the input data
     with open('../data/hamm_test.txt') as infile:
+        # Each readline reads exactly one line, and then moves to next.
         seq1 = infile.readline().strip()
         seq2 = infile.readline().strip()
         print(hamming(seq1, seq2))
--- scripts/iprb.py
+++ scripts/iprb.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def mendel_pair(k, m, n):
+    """Return probability of dominant offspring from 2 indivs. in pop k+m+n"""
     t = k + m + n
+    # To calculate this, just use a decision tree...
     return 1 - (1/(t*(t-1)))*(0.25*m*(m-1) + m*n + n*(n-1))
 
 if __name__ == '__main__':
